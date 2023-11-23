f44bb88da2eac2d0a122a6cc158e4e9562d5bdf1
Sudospective <admin@sudospective.net>
2023-11-22 21:54:14 -0800

    Create tweens.lua

--- src/main.lua
+++ src/main.lua
@@ -1,36 +1,36 @@
 include 'actors'
 
 
-local shaders = ShaderLoader:new()
-local bg = Rect:new()
+Shaders = ShaderLoader:new()
+BG = Rect:new()
 
-local proxy = {}
+Proxies = {}
 for _, pn in ipairs(Players) do
-	proxy[pn] = Proxy:new()
+	Proxies[pn] = Proxy:new()
 end
 
-local r = Rect:new()
-local i = Image:new()
-local l = Label:new()
-local l2 = Label:new()
+R = Rect:new()
+I = Image:new()
+L = Label:new()
+L2 = Label:new()
 
 
 function init()
 
-	shaders:LoadShader(SRC_ROOT..'/assets/breathing-colors.frag', 'Colors')
+	Shaders:LoadShader(SRC_ROOT..'/assets/breathing-colors.frag', 'Colors')
 
 end
 
 function ready()
 
-	bg:FullScreen():SetShader(shaders.Colors):glow(0, 0, 0, 0.5)
+	BG:FullScreen():SetShader(Shaders.Colors):glow(0, 0, 0, 0.5)
 
 	for _, pn in ipairs(Players) do
-		proxy[pn]:SetTarget(ActorTable[pn])
+		Proxies[pn]:SetTarget(ActorTable[pn])
 		ActorTable[pn]:visible(false)
 	end
 
-	r:name('GoodBoy')
+	R:name('GoodBoy')
 		:Center()
 		:SetSize(96, 96)
 		:zoom(0)
@@ -39,7 +39,7 @@ function ready()
 		:zoom(1)
 		:rotationz(-45)
 	
-	i:name('Strawb')
+	I:name('Strawb')
 		:Center()
 		:Load(SRC_ROOT..'/assets/strawb.png')
 		:SetSize(64, 64)
@@ -47,7 +47,7 @@ function ready()
 		:easeoutback(0.25)
 		:zoom(1)
 	
-	l:name('Ichigo')
+	L:name('Ichigo')
 		:LoadFromFont(THEME:GetPathF('Common', 'Normal'))
 		:Center()
 		:diffuse(0, 0, 0, 1)
@@ -58,7 +58,7 @@ function ready()
 		:easeoutback(0.25)
 		:zoom(1)
 
-	l2:name('Template')
+	L2:name('Template')
 		:LoadFromFont(THEME:GetPathF('Common', 'Normal'))
 		:Center()
 		:addy(96)
@@ -72,31 +72,10 @@ end
 
 function update(params)
 
-	r:addrotationz(params.dt * -90)
+	R:addrotationz(params.dt * -90)
 
 end
 
 
-ease {9, function()
-		
-	r:stoptweening()
-		:easeinquad(0.5)
-		:addrotationz(-360)
-		:zoom(0)
-	
-	i:stoptweening()
-		:easeinback(0.25)
-		:zoom(0)
-	
-	l:stoptweening()
-		:easeinback(0.25)
-		:zoom(0)
-	
-	l2:stoptweening()
-		:linear(0.25)
-		:cropright(1)
-	
-end}
-
-
 run '/gimmicks.lua'
+run '/tweens.lua'
---
+++ src/tweens.lua
@@ -0,0 +1,20 @@
+ease {9, function()
+		
+	R:stoptweening()
+		:easeinquad(0.5)
+		:addrotationz(-360)
+		:zoom(0)
+	
+	I:stoptweening()
+		:easeinback(0.25)
+		:zoom(0)
+	
+	L:stoptweening()
+		:easeinback(0.25)
+		:zoom(0)
+	
+	L2:stoptweening()
+		:linear(0.25)
+		:cropright(1)
+	
+end}
