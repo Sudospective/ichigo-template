878232050f1ca20e2cbc32acd716737636a8e35a
Sudospective <admin@sudospective.net>
2023-11-27 15:48:37 -0800

    Change some gimmicks to player 1

--- src/gimmicks.lua
+++ src/gimmicks.lua
@@ -12,7 +12,7 @@
 ease
 	{0, 2, 'xmod'}
 	{0, 100, 'modtimersong'}
-	{0, 2, Tweens.easeOutBounce, 0, 100, 'flip', plr = 2}
-	{0, 4, Tweens.easeOutElastic, 0, -100, 'invert', plr = 2}
+	{0, 2, Tweens.easeOutBounce, 0, 100, 'flip', plr = 1}
+	{0, 4, Tweens.easeOutElastic, 0, -100, 'invert', plr = 1}
 	{0, 4, Tweens.easeOutElastic, 100, 0, 'drunk'}
 	{0, 6, Tweens.easeOutElastic, 100, 0, 'tipsy'}
