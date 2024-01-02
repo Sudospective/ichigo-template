39efa8e43f649636761cfbaf9c2648dfa203cebe
Sudospective <admin@sudospective.net>
2024-01-02 11:32:37 -0800

    a little more helpful of an example

--- src/include/example.lua
+++ src/include/example.lua
@@ -1,11 +1,14 @@
 if Example then return end -- this is our load blocker
 
+-- base class
 class 'Example' {
 	Field = true,
 	Method = function(self) return true end
 }
 
+-- derived class
 class 'Example2' : extends 'Example' {
 	NewField = 'foo',
+	Method = function(self) return false end
 	NewMethod = function(self) return 'foo' end
 }
