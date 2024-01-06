f73eec7e90ccaa01d7fb770b8c758ee29bc3f5af
Sudospective <admin@sudospective.net>
2024-01-06 13:35:01 -0800

    Add legacy modreader support

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ setmetatable(ichi, {
 })
 
 
-ichi.__version = '1.0-RC1'
+ichi.__version = '1.0-RC2'
 ichi.ichi = ichi
 ichi.Actors = Def.ActorFrame {}
 ichi.SONG = GAMESTATE:GetCurrentSong()
--- lib/std.lua
+++ lib/std.lua
@@ -98,13 +98,14 @@ function ichi.RegisterOptions(po)
 end
 
 
-return Def.PandaTemplate {
+return (ProductVersion():find('0.5')) and Def.PandaTemplate {
 	Name = 'Bookworm',
 	ClearDoneMods = true,
 	ClearDoneEases = true,
 	ClearAllPoptions = true,
 	LoopModsAllPoptions = true,
 	OnCommand = function(self)
+		print('Using PandaTemplate Modreader')
 		self:PopulateBeatMods(ModTable)
 		self:PopulateBeatMessages(MsgTable)
 		self:PopulateEases(EaseTable)
@@ -113,5 +114,104 @@ return Def.PandaTemplate {
 	end,
 	UpdateCommand = function(self, params)
 		self:SetUpdateSleep(params.dt)
+	end
+} or Def.ActorFrame {
+	Name = 'Bookworm',
+	OnCommand = function(self)
+		print('Using Legacy Modreader')
+		self.Disable = false
+		self.FirstBeat = ichi.SONG_POS:GetSongBeat()
+		self.CurAction = 1
+		local function mod_compare(a, b)
+			return a[1] < b[1]
+		end
+		if #MsgTable > 1 then
+			table.sort(MsgTable, mod_compare)
+		end
+		self:queuecommand('Update')
+	end,
+	UpdateCommand = function(self)
+		-- help
+		local beat = ichi.SONG_POS:GetSongBeat()
+		if not self.Disable then
+			if beat > self.FirstBeat + 0.1 then
+				-- reset
+				for pn = 1, #PopTable do
+					PopTable[pn]:FromString('clearall')
+				end
+				-- mods
+				for i, v in ipairs(ModTable) do
+					if v and #v > 3 and v[1] and v[2] and v[3] and v[4] then
+						if beat >= v[1] then
+							if (v[4] == 'len' and beat <= v[1] + v[2]) or (v[4] == 'end' and beat <= v[2]) then
+								if #v == 5 and v[5] then
+									PopTable[v[5]]:FromString(v[3])
+								else
+									for pn = 1, #PopTable do
+										PopTable[pn]:FromString(v[3])
+									end
+								end
+							end
+						end
+					else
+						v[1] = 0
+						v[2] = 0
+						v[3] = ''
+						v[4] = 'error'
+						SCREENMAN:SystemMessage('Bad mod in beat-based table (line '..i..')')
+					end
+				end
+				-- eases
+				for i, v in ipairs(EaseTable) do
+					if v and #v > 6 and v[1] and v[2] and v[3] and v[4] and v[5] and v[6] and v[7] then
+						if beat >= v[1] then
+							if (v[6] == 'len' and beat <= v[1] + v[2]) or (v[6] == 'end' and beat <= v[2]) then
+								local strength = v[7](beat - v[1], v[3], v[4] - v[3], v[6] == 'end' and v[2] - v[1] or v[2], v[10], v[11])
+								if type(v[5]) == 'string' then
+									local modstr = v[5] == 'xmod' and strength..'x' or (v[5] == 'cmod' and 'C'..strength or strength..' '..v[5])
+									if v[8] then
+										PopTable[v[8]]:FromString('*9e9 '..modstr)
+									else
+										for pn = 1, #PopTable do
+											PopTable[pn]:FromString('*9e9 '..modstr)
+										end
+									end
+								elseif type(v[5]) == 'function' then
+									v[5](strength)
+								end
+							elseif (v[9] and ((v[6] == 'len' and beat <=v[1] + v[2] + v[9]) or (v[6] == 'end' and beat <= v[9]))) then
+								if type(v[5]) == 'string' then
+									local modstr = v[5] == 'xmod' and v[4]..'x' or (v[5] == 'cmod' and 'C'..v[4] or v[4]..' '..v[5])
+									if v[8] then
+										PopTable[v[8]]:FromString('*9e9 '..modstr)
+									else
+										for pn = 1, #PopTable do
+											PopTable[pn]:FromString('*9e9 '..modstr)
+										end
+									end
+								elseif type(v[5]) == 'function' then
+									v[5](v[4])
+								end
+							end
+						end
+					else
+						SCREENMAN:SystemMessage('Ease Error! (line '..i..' | beat: '..v[1]..' | mod: '..v[5]..')')
+					end
+				end
+				-- actions
+				while self.CurAction <= #MsgTable and beat >= MsgTable[self.CurAction][1] do
+					local action = MsgTable[self.CurAction]
+					if action[3] or beat < action[1] + 2 then
+						if type(action[2]) == 'function' then
+							action[2]()
+						elseif type(action[2]) == 'string' then
+							MESSAGEMAN:Broadcast(action[2])
+						end
+					end
+					self.CurAction = self.CurAction + 1
+				end
+			end
+		end
+		self:sleep(self:GetEffectDelta()):queuecommand('Update')
 	end
 }
