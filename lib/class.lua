52ac22f29d46e86d233455915d89bcf8b899aa87
Sudospective <admin@sudospective.net>
2024-02-19 17:13:47 -0800

    Move ichi.namespace to class library

--- lib/class.lua
+++ lib/class.lua
@@ -31,3 +31,18 @@ function ichi.class(name)
   end
   return setmetatable(newclass,{__call=newclass.define})
 end
+
+function ichi.namespace(name)
+  local newspace = {}
+  ichi[name] = newspace
+  return function(t)
+    for k, v in pairs(t) do
+      if type(v) == 'table' then
+        newspace[k] = DeepCopy(v)
+      else
+        newspace[k] = v
+      end
+    end
+    return setmetatable(newspace, {__index = newspace})
+  end
+end
